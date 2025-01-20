cca2608279201ce34eafedbd61fbe3dfb253f064
MasterPhooey <106418429+MasterPhooey@users.noreply.github.com>
2025-01-20 14:00:07 -0600

    Update setup_notebooks.sh

--- setup_notebooks.sh
+++ setup_notebooks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Check if the Piper training notebook exists in /data
-if [ ! -f /data/piper_training_notebook.ipynb ]; then
+if [ ! -f /data/train.ipynb ]; then
     echo "Piper training notebook not found in /data. Copying the default notebook..."
-    cp /root/piper_training_notebook.ipynb /data/piper_training_notebook.ipynb
+    cp /root/train.ipynb /data/train.ipynb
 else
     echo "Piper training notebook already exists in /data. Skipping copy."
 fi
